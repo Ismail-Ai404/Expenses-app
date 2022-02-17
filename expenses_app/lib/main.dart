72ea307e5e75b636b3f829f30274ea68723d28c4
Smilexix_ <ayon000777@gmail.com>
2022-02-17 14:36:02 +0600

    Add list of dummy transactions

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,14 +15,56 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Buy milk',
+      amount: 59.99,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text(
+          'Flutter App',
+          textAlign: TextAlign.center,
+        ),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Colors.blue[200],
+            child: Container(
+              child: Text(
+                'Hello',
+                textScaleFactor: 3,
+              ),
+              width: double.infinity,
+            ),
+            elevation: 5,
+          ),
+          Container(
+            width: 120,
+            child: Card(
+              elevation: 5,
+              color: Colors.red[300],
+              child: Text(
+                'Another one!',
+                textScaleFactor: 2,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
